88e4ce1f5209a0ccec85373f18633770b5d6b6db
itssinghnakit <singhankit.kr@com.gmail>
2022-10-11 23:56:55 +0530

    project completion

--- js/validation.js
+++ js/validation.js
@@ -1,107 +1,139 @@
-function validate(){
-    var y=false;
-    var a=false;
-    var b=false;
-    var c=false;
-    var d=false;
-    var e=false;
-    var f=false;
+var a = false;
+var a = false;
+var b = false;
+var c = false;
+var d = false;
+var e = false;
+var f = false;
 
-    clearerror();
-    let fname=document.getElementById("First_Name").value;
-    let lname=document.getElementById("Last_Name").value;
-    let mobile=document.getElementById("Mobile_No").value;
-    let email=document.getElementById("Email_Address").value;
-    let pass=document.getElementById("Password").value;
-    let cpass=document.getElementById("C_Password").value;
-    let cours=document.getElementById('course').value;
-    
- 
-    let mobilereg=/^\d{10}$/;
-    let fnamereg=/^[a-zA-Z]+$/;
-    let lnamereg=/^[a-zA-Z]+$/;
-    let emailreg=/^([a-zA-Z0-9\.-_]+)@([a-zA-Z]+).([a-zA-Z]{2,4})$/;
-    let passreg=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+document.getElementById('First_Name').addEventListener("blur", () => {
 
-    if(fnamereg.test(fname))
-    {
-        a=true;
+    let fname = document.getElementById("First_Name").value;
+    let fnamereg = /^[a-zA-Z]+$/;
+
+    if (fnamereg.test(fname)) {
+        a = true;
+        clearerror();
     }
-    else{
-    let element=document.getElementById('First_Name');
-    element.parentNode.nextElementSibling.innerHTML="First name should have alphabets only";
-    a=false;
-     }
-
-    if(lnamereg.test(lname))
-    {
-        b=true;
+    else {
+        let element = document.getElementById('First_Name');
+        element.parentNode.nextElementSibling.innerHTML = "First name should have alphabets only";
+        a = false;
     }
-    else{
-    let element=document.getElementById('Last_Name');
-    element.parentNode.nextElementSibling.innerHTML="Last name should have alphabets only";
-    b=false;
+});
+
+document.getElementById('Last_Name').addEventListener("blur", () => {
+
+    let lname = document.getElementById("Last_Name").value;
+    let lnamereg = /^[a-zA-Z]+$/;
+
+    if (lnamereg.test(lname)) {
+        b = true;
+        clearerror();
+    }
+    else {
+        let element = document.getElementById('Last_Name');
+        element.parentNode.nextElementSibling.innerHTML = "Last name should have alphabets only";
+        b = false;
+    }
+})
+
+document.getElementById('Mobile_No').addEventListener("blur", () => {
+
+    let mobile = document.getElementById("Mobile_No").value;
+    let mobilereg = /^\d{10}$/;
+
+    if (mobilereg.test(mobile)) {
+        c = true;
+        clearerror();
+    }
+    else {
+        let element = document.getElementById('Mobile_No');
+        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML = "Mobile number should be 10 digit number";
+        c = false;
     }
 
+})
 
-    if(mobilereg.test(mobile))
-    {
-       c=true;
+document.getElementById('Email_Address').addEventListener("blur", () => {
+
+    let email = document.getElementById("Email_Address").value;
+    let emailreg = /^([a-zA-Z0-9\.-_]+)@([a-zA-Z]+)\.([a-zA-Z]{2,4})$/;
+
+    if (emailreg.test(email)) {
+        d = true;
+        clearerror();
     }
-    else{
-        let element=document.getElementById('Mobile_No');
-        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML="Mobile number should be 10 digit number";
-        c=false;
+    else {
+        let element = document.getElementById('Email_Address');
+        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML = "Email should be of form abc.cd@gmail.com";
+        d = false;
+    }
+
+})
+
+document.getElementById('Password').addEventListener("blur", () => {
+
+    let pass = document.getElementById("Password").value;
+    let passreg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+    if (passreg.test(pass)) {
+        e = true;
+        clearerror();
+    }
+    else {
+        let element = document.getElementById('Password');
+        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML = "Password should be atleast 8 letters containing capital letter,small letter, number and special character";
+        e = false;
+    }
+})
+
+document.getElementById('C_Password').addEventListener("blur", () => {
+
+    let cpass = document.getElementById("C_Password").value;
+    let pass = document.getElementById("Password").value;
+
+    if (cpass == pass) {
+        f = true;
+        clearerror();
+    }
+    else {
+        let element = document.getElementById('C_Password');
+        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML = "Both Password should match";
+        f = false;
+    }
+
+})
+
+function clearerror() {
+    let errors = document.getElementsByClassName('displayerror');
+    for (let i of errors) {
+        i.innerHTML = "";
+    }
 }
 
-    if(emailreg.test(email))
-    {
-       d=true;
+// After Submit Button
+
+function validate() {
+
+    let fname = document.getElementById("First_Name").value;
+    let lname = document.getElementById("Last_Name").value;
+    let mobile = document.getElementById("Mobile_No").value;
+    let email = document.getElementById("Email_Address").value;
+    let pass = document.getElementById("Password").value;
+    let cpass = document.getElementById("C_Password").value;
+    let cours = document.getElementById('course').value;
+
+    var y = false;
+    y = a && b && c && d && e && f;
+    if (y == true) {
+        console.log(fname);
+        console.log(lname);
+        console.log(mobile);
+        console.log(email);
+        console.log(cours);
+        alert("Your Form Is Successfully Submitted");
     }
-    else{
-        let element=document.getElementById('Email_Address');
-        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML="Email should be of form abc.cd@gmail.com";
-        d=false;
-}
-
-    if(passreg.test(pass))
-    {
-       e=true;
-    }
-    else{
-        let element=document.getElementById('Password');
-        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML="Password should be atleast 8 letters containing capital letter,small letter, number and special character";
-        e=false;
-        }
-
-    if(cpass==pass)
-    {
-       f=true;
-    }
-    else{
-        let element=document.getElementById('C_Password');
-        element.parentNode.getElementsByClassName('displayerror')[0].innerHTML="Both Password should match";
-        f=false;
-}
-
-
-if(a&&b&&c&&d&&e&&f){
-    console.log(fname);
-    console.log(lname);
-    console.log(mobile);
-    console.log(email);
-    console.log(cours);
-    y=true;
     return y;
- }
-
-function clearerror(){
-    let errors=document.getElementsByClassName('displayerror');
-    for(let i of errors)
-    {
-        i.innerHTML="";
-    }
-}
-return y;
 
 }
